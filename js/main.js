d83fa1fa59e2ebded1997fc53fbff3b758fc25d4
J-Massoda <emilienmassoda@gmail.com>
2022-11-08 13:36:41 +0100

    Not a bad start.
    Edited image names,
    50% footer complete.
    carousel slide show adjusted.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function showMenu() {
+    document.querySelector('#nav-links').style.right = "0";
+}
+function hideMenu() {
+    document.querySelector('#nav-links').style.right = "-200px";
+}
+
+// Slide show ........................................................................
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
